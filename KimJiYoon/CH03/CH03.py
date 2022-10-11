9c5fb7d765c2f4be48d1d66178157247abbe66d6
JiYoonKim <badjiyoon1@gmail.com>
2022-10-11 22:45:17 +0900

    김지윤 / 10까지 완료

--- KimJiYoon/CH03/CH03.py
+++ KimJiYoon/CH03/CH03.py
@@ -163,3 +163,172 @@ print(df[a])
 print(df[(df['a'] <= 3) | (df['a'] >= 7)])
 # a가 3이상이고 b가 16미만이거나 c가 30인 데이터
 print(df[(df['a'] >= 3) & ((df['b'] < 16) | (df['c'] == 30))])
+
+# 정렬
+df = pd.DataFrame({'a': [2, 3, 2, 7, 4], 'b': [2, 1, 3, 5, 3], 'c': [1, 1, 2, 3, 5]})
+print(df)
+print(df.sort_index())
+print(df.sort_index(ascending=False))
+# 저장 시 inplace True
+df.sort_index(ascending=False, inplace=True)
+print(df)
+# 인덱스만 초기화
+print(df.reset_index())
+# 인덱스 삭제
+print(df.reset_index(drop=True))
+# 인덱스 삭제 및 적용
+df.reset_index(drop=True, inplace=True)
+print(df)
+# 값 기준 정렬
+df = pd.DataFrame({'a': [2, 3, 2, 7, 4], 'b': [2, 1, 3, 5, 3], 'c': [1, 1, 2, 3, 5]})
+print(df)
+
+# a열 기준으로 오름차순 정렬을하시오
+print(df.sort_values(by=['a']))
+df.sort_values(by=['a'], inplace=True)
+print(df)
+print(df.sort_values(by=['a'], ascending=False))
+# a,b 열 기준으로 오름차순 정렬하시오
+print(df.sort_values(by=['a', 'b']))
+# a열 기준으로 오름차순 정렬한 이후, b열 기준으로 내림차순 정렬하시오.
+print(df.sort_values(by=['a', 'b'], ascending=[True, False]))
+df.reset_index(drop=True, inplace=True)
+
+# 결측값 처리
+df = pd.DataFrame({'a': [1, 1, 3, 4, 5], 'b': [2, 3, np.nan, 3, 4], 'c': [3, 4, 7, 6, 4]})
+print(df)
+# 결측 유무 확인
+print(df.isnull())
+# 결측값 개수 확인
+print(df.isnull().sum())
+# 결측값이 포함된 행 지우기
+print(df.dropna())
+df.dropna(inplace=True)
+print(df)
+# 결측값이 포함된 열 지우기
+df = pd.DataFrame({'a': [1, 1, 3, 4, 5], 'b': [2, 3, np.nan, 3, 4], 'c': [3, 4, 7, 6, 4]})
+print(df.dropna(axis=1))
+df.dropna(axis=1, inplace=True)
+print(df)
+# 결측값을 다른값으로 대체하기
+df = pd.DataFrame({'a': [1, 1, 3, 4, 5], 'b': [2, 3, np.nan, 3, 4], 'c': [3, 4, 7, 6, 4]})
+print(df)
+df.fillna(0, inplace=True)
+print(df)
+# 열이나 뒤의 숫자로 바꾸기
+df = pd.DataFrame({'a': [1, 1, 3, 4, np.nan], 'b': [2, 3, np.nan, np.nan, 4], 'c': [np.nan, 4, 7, 6, 4]})
+print(df)
+# 뒤의 값으로 채우기
+print(df.fillna(method='bfill'))
+print(df.fillna(method='ffill'))
+# limit 설정
+print(df.fillna(method='ffill', limit=1))
+# 데이터 프레임에 존재하는 결측값들을 뒤의 값으로 대체한 이후 앞의 값으로 대체하시오
+df = pd.DataFrame({'a': [1, 1, 3, 4, np.nan], 'b': [2, 3, np.nan, np.nan, 4], 'c': [np.nan, 4, 7, 6, 4]})
+print(df)
+df.fillna(method='bfill', inplace=True)
+df.fillna(method='ffill', inplace=True)
+print(df)
+# 평균으로 대체
+df = pd.DataFrame({'a': [1, 1, 3, 4, np.nan], 'b': [2, 3, np.nan, np.nan, 4], 'c': [np.nan, 4, 1, 1, 4]})
+print(df)
+df.mean()['a']
+print(df.fillna(df.mean()['a']))
+df.mean()
+print(df.fillna(df.mean()))
+print(df)
+print(df.fillna(df.mean()[['b', 'c']]))
+
+# 타입 변환
+df = pd.DataFrame({
+    '판매일': ['5/11/21', '5/12/21', '5/13/21', '5/14/21', '5/15/21'],
+    '판매량': ['10', '15', '20', '25', '30'],
+    '방문자수': ['10', '-', '17', '23', '25'],
+    '기온': ['24.1', '24.3', '24.8', '25', '25.4']
+})
+print(df)
+# 타입 확인
+print(df.dtypes)
+# 1더하는게 안됨
+# df['판매량 보정'] = df['판매량'] + 1
+df.astype({'판매량': 'int'})
+print(df.dtypes)
+df = df.astype({'판매량': 'int'})
+print(df.dtypes)
+df['판매량 보정'] = df['판매량'] + 1
+print(df)
+# 방문자수를 숫자 타입으로 변환하시오
+# 옵션 써야함
+# df.astype({'방문자수': 'int'})
+df['방문자수'] = pd.to_numeric(df['방문자수'], errors='coerce')
+print(df.dtypes)
+print(df)
+# 에러남 nan 때문에
+# df = df.astype({'방문자수': 'int'})
+df.fillna(0, inplace=True)
+df = df.astype({'방문자수': 'int'})
+print(df)
+print(df.dtypes)
+# 판매일을 datetime의 형태로 바꾸시오
+df['판매일'] = pd.to_datetime(df['판매일'], format='%m/%d/%y')
+print(df)
+print(df.dtypes)
+
+# 타입 변환
+df = pd.DataFrame({
+    'a': [1, 1, 3, 4, 5],
+    'b': [2, 3, 2, 3, 4],
+    'c': [3, 4, 7, 6, 4]
+})
+
+# 1, 3, 6, 4, 8로 이루어진 d칼럼을 추가하시오
+df['d'] = [1, 3, 6, 4, 8]
+print(df)
+# 1로 이루어진 e 컬럼을 추가하시오
+df['e'] = [1, 1, 1, 1, 1]
+print(df)
+df['e'] = 1
+print(df)
+print(df.dtypes)
+# a + b - c의 결과로 이루어진 f컬럼을 추가하시오
+df['f'] = df['a'] + df['b'] - df['c']
+print(df)
+# 컬럼 d, e, f를 삭제하시오
+df.drop(['d', 'e', 'f'], axis=1)
+print(df)
+df.drop(['d', 'e', 'f'], axis=1, inplace=True)
+print(df)
+# 레코드 추가
+# a에는 6, b에는 7, c에는 8을 추가하시오
+# 에러 인덱스 무시옵션 처리해야함
+# df.append({'a': 6, 'b': 7, 'c': 8})
+df.append({'a': 6, 'b': 7, 'c': 8}, ignore_index=True)
+print(df)
+df = df.append({'a': 6, 'b': 7, 'c': 8}, ignore_index=True)
+print(df)
+
+# a에는 7, b에는 8, c에는 9를 추가하시오
+df.loc[6] = [7, 8, 9]
+print(df)
+# 레코드삭제
+# 첫 번쨰 레코드를 삭제하시오
+print(df)
+df.drop(0)
+print(df)
+df = df.drop([0, 1])
+print(df)
+# 첫 번쨰에서 네 번째 레코드를 삭제하시오
+df = pd.DataFrame({'a': [1, 1, 3, 4, 5], 'b': [2, 3, 2, 3, 4], 'c': [3, 4, 7, 6, 4]})
+print(df)
+print(df.drop([i for i in range(4)]))
+print(df)
+print(df.drop(df.index[:4]))
+# a가 4미만인 레코드들을 삭제하시오
+print(df)
+print(df[df['a'] < 4].index)
+print(df.drop(df[df['a'] < 4].index))
+# a가 3미만이고 c가 4인 레코드를 삭제하시오
+print(df)
+df[(df['a'] < 3) & (df['c'] == 4)].index
+print(df.drop(df[(df['a'] < 3) & (df['c'] == 4)].index))
+

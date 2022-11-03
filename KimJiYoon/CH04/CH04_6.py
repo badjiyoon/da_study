6c13de664d4b1c09e530e6603fd3a310b44b0b37
JiYoonKim <badjiyoon1@gmail.com>
2022-11-03 21:56:47 +0900

    김지윤 / 6강까지 완료

--- KimJiYoon/CH04/CH04_6.py
+++ KimJiYoon/CH04/CH04_6.py
@@ -1,4 +1,21 @@
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import seaborn as sns
+import pandas as pd
+sns.set(rc={'figure.figsize': (10, 5)})
 
+df = sns.load_dataset('titanic')
+# 성별 인원 수 처리
+print(df.head())
+print(df.describe())
+print(df.isnull().sum())
+print(df.groupby(by='sex')['sex'].count())
+# sns.countplot(data=df, x='sex')
+# sns.countplot(data=df, y='sex')
+# 성별 인원수를 객실 등급별로 시각화
+print(df[['sex','class']])
+print(df.groupby(by=['sex', 'class'])['sex'].count())
+# sns.countplot(data=df, x='sex', hue='class')
+# palette를 사용한 색상조정
+sns.countplot(data=df, x='sex', hue='class', palette='flare')
+plt.show()

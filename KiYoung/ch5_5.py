30dd9480a25a2eb94fe0ea57edcbc4b054095acf
jk <jky20051@gmail.com>
2022-11-13 08:44:19 +0900

    커밋

---
+++ KiYoung/ch5_5.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([[1, 2], [3, 4]])
+
+
+a = np.array([[1,2,3], [4,5,6]])
+b = np.array([[2,3,4], [5,6,7]])
+a = np.array([[1,2,3], [4,5,6]])
+b = np.array([[1,2],[3,4],[5,6]])
+
+
+
+print(np.dot(a,b))

b7f7e3310e2744833809f31a614e8701af4ef70c
JiYoonKim <badjiyoon1@gmail.com>
2022-11-13 22:20:13 +0900

    김지윤 / 오타수정

--- KimJiYoon/CH05/CH05_7.py
+++ KimJiYoon/CH05/CH05_7.py
@@ -20,7 +20,7 @@ print(y_test[0])
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0, 2),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
